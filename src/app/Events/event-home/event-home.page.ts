d2b58e8438b8be4cbe395e12146a756126593b0a
bryce613 <brycen.cloward@gmail.com>
2019-05-01 12:35:16 -0600

    Update event-home.page.ts

--- src/app/Events/event-home/event-home.page.ts
+++ src/app/Events/event-home/event-home.page.ts
@@ -12,32 +12,11 @@ export class EventHomePage implements OnInit {
 
   public arr: any;
   public todaysDate: Date = new Date('April 2, 2019 03:24:00');
-  public oneOut: Date;
-  public twoOut: Date;
-  public threeOut: Date;
-  public fourOut: Date;
-  public fiveOut: Date;
-  public sixOut: Date;
-  public sevenOut: Date;
   
   constructor(public events: EventsDataService, public navCtrl: NavController, public router: Router) { 
     this.arr = this.events.xmlItems;
     console.log("Starting arr...");
     console.log(this.arr);
-    this.oneOut = new Date();
-    this.twoOut = new Date();
-    this.threeOut = new Date();
-    this.fourOut = new Date();
-    this.fiveOut = new Date();
-    this.sixOut = new Date();
-    this.sevenOut = new Date();
-    this.oneOut.setDate(this.oneOut.getDate() + 1);
-    this.twoOut.setDate(this.twoOut.getDate() + 2);
-    this.threeOut.setDate(this.threeOut.getDate() + 3);
-    this.fourOut.setDate(this.fourOut.getDate() + 4);
-    this.fiveOut.setDate(this.fiveOut.getDate() + 5);
-    this.sixOut.setDate(this.sixOut.getDate() + 6);
-    this.sevenOut.setDate(this.sevenOut.getDate() + 7);
   }
 
   goEventDetails(theEventData){
@@ -61,20 +40,6 @@ export class EventHomePage implements OnInit {
     this.arr = this.events.xmlItems;
     console.log("Starting arr...");
     console.log(this.arr);
-    this.oneOut = new Date();
-    this.twoOut = new Date();
-    this.threeOut = new Date();
-    this.fourOut = new Date();
-    this.fiveOut = new Date();
-    this.sixOut = new Date();
-    this.sevenOut = new Date();
-    this.oneOut.setDate(this.oneOut.getDate() + 1);
-    this.twoOut.setDate(this.twoOut.getDate() + 2);
-    this.threeOut.setDate(this.threeOut.getDate() + 3);
-    this.fourOut.setDate(this.fourOut.getDate() + 4);
-    this.fiveOut.setDate(this.fiveOut.getDate() + 5);
-    this.sixOut.setDate(this.sixOut.getDate() + 6);
-    this.sevenOut.setDate(this.sevenOut.getDate() + 7);
   }
 
   getEvents(event)
